8715dcded44a1cdac6d9849d4f2d971f77c02889
panduharry <pandu@indah.id>
2026-04-11 19:08:43 +0700

    Product::detail > go review

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -66,7 +66,7 @@
     <div class="min-h-screen flex flex-col">
         <!-- Main Content -->
         <main class="flex-1 flex items-center justify-center p-6">
-            <div class="w-full max-w-lg">
+            <div class="w-full max-w-md">
                 {{ $slot }}
             </div>
         </main>
